437c6c3c666025a21d36eaed68b168c052d5578e
razamehdisyed <razamehdisyed@gmail.com>
2023-03-24 10:15:30 +0500

    after root reducer

--- src/containers/HomeContainer.js
+++ src/containers/HomeContainer.js
@@ -1,4 +1,16 @@
-import React from "react";
+import {connect} from 'react-redux'
 import Home from '../components/Home'
+import {addToCart} from '../Services/Actions/actions'
 
-export default Home;
+
+const mapStateToProps=state=>({
+
+})
+const mapDispatchToProps=dispatch=>({
+    addToCartHandler:data=>dispatch(addToCart(data))
+})
+export default connect(mapStateToProps,mapDispatchToProps)(Home)
+
+
+
+//export default Home;
